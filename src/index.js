e76b6628a8bdf0814b123dbd893e554d1ac6e7cd
Vladimir Hoovenko <holovenk@gmail.com>
2021-03-15 16:14:00 +0700

    first day of learn webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as $ from "jquery"
+import Post from "./Post"
+import json from "./assets/test.json"
+import xml from "./assets/test.xml"
+import webpack_logo from "./assets/webpack-logo.png"
+import "./styles/style.css"
+
+const post = new Post("Webpack Learn", webpack_logo);
+
+$("pre").html(post.toString())
+
+console.log("JSON: ",json)
+console.log("XML: ",xml)
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    entry: {
+        main: "./index.js",
+        analytics: "./analytics.js"
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html"
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+                patterns: [
+                    {from: path.resolve(__dirname, "src/favicon.ico"), to: path.resolve(__dirname, "dist")}
+                ]
+            }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{loader: MiniCssExtractPlugin.loader, options: {}}, "css-loader"]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.ttf$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.xml$/,
+                use: ["xml-loader"]
+            }
+        ]
+    }
+}
